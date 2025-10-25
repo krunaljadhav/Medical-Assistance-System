4aceb1c9f5fc8f7472976abe8e32869500c8ee82
krunaljadhav <krunalj91@gmail.com>
2025-10-26 03:42:26 +0530

    requirments added from setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-Assistance-System",
+    version="0.1.0",
+    author="Krunal Jadhav",
+    author_email="krunalj91@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
